2e10f456788dace0b156b08745508fc41f587037
Matheus <matheus.nmt@gmail.com>
2024-09-23 23:37:42 -0300

    Criado o arquivo header (.hpp) para implementar a classe que processará os dados de entrada

---
+++ src/processInputs.hpp
@@ -0,0 +1,77 @@
+
+#include <string>
+#include <map>
+
+/*
+                                                                CLASSE processInputs
+    
+    Como o próprio nome já diz, a classe terá funções membro (métodos) que realizarão uma interface com a função main no processamento dos argumentos de entrada via SO;
+
+    Essas funções membro manipularão variáveis membro (atributos) que conterão maps com os arquivos de entrada, o nome do arquivo de entrada e o formato de saída.
+
+    A função membro que processará os dados de entrada deve levar em consideração que:
+        * Os argumentos podem vir em ordem sortida;
+        * Podem não haver argumetos, sendo necessário tratar a ausência de parâmetros adicionais para método de ordenação e formato de saída;
+        * Avisar ao usuário caso ele envie parâmetros não aceitáveis, elaborando um tipo de --help
+    
+    A classe também deverá implementar o padrão Unicode.
+
+    Após as funções membro da classe processarem todos os dados de entrada, uma segunda classe ordenará e organizará o map em um vector de tuplas.
+
+
+*/
+
+
+#ifndef PROCESSINPUTS_HPP
+
+#define PROCESSINPUTS_HPP
+
+namespace inp{
+
+class processInputs{
+
+    private:
+
+        std::string modo_ordenacao;
+
+        std::string nome_arquivo;
+
+        std::string formato_saida;
+
+        std::map<std::wstring, int> ocorrencias;
+
+    public:
+
+        processInputs(); //construtor sem parâmetros, pois os dados deverão ser processados por outras funções, não necessariamente na hora da declaração do objeto.
+
+        void processa_argumentos(int argc, char **argv);
+
+        void processa_entrada(std::string arquivo);
+
+        
+        //funções de get:
+
+        std::string get_modo_ordenacao(){
+            return modo_ordenacao;
+        }
+
+        std::string get_formato_saida(){
+            return formato_saida;
+        }
+
+        //funções de set não serão implementadas pois as variáveis membro serão alteradas por dentro da classe.
+
+};
+
+}
+
+#endif
+
+
+/*       
+
+    To Do: implementar funções para processar os argumentos recebidos do SO;
+           implementar função que lê o arquivo de texto e insere seu conteúdo em um map;
+           não esquecer que essa classe deverá permitir que seja feita a comunicação com outras classes por meio de seus objetos;
+
+*/
