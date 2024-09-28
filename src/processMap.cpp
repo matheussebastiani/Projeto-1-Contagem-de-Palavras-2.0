bca57c7bc5d46895a1e296e0ae14dd07e23fd222
Matheus <matheus.nmt@gmail.com>
2024-09-28 17:34:25 -0300

    Adicionados novos arquivos

---
+++ src/processMap.cpp
@@ -0,0 +1,51 @@
+#include "processMap.hpp"
+
+
+/*
+
+                                            Aqui ficará a implementação das funções-membro da classe processMap.
+
+*/
+
+namespace mid{
+
+    
+    //TO DO: implementar função membro pushMap
+
+
+    /*
+
+        Escopo da função membro pushMap():
+        A função membro pushMap receberá o map da classe inp::processInputs e será responsável por iterar item a item do map contendo as palavras lidas,
+        e fará a inserção dos campos do map em um vector de ponteiros para pair, estrutura que possui dois atributos: first e second. 
+        O (pair-> first) armazenará a chave do map recebido, e o (pair->second) receberá a quantidade de ocorrências da palavra (pair->first).
+        Essa ação é necessária para posteriormente iterarmos com o vector de pairs e ordená-lo por meio da função sort_vector(), da maneira escolhida pelo usuário
+
+    */
+
+    void pushMap(std::map<std::wstring, int> palavrasLidas){
+
+
+    }
+
+
+    //TO DO: implementar a função membro sort_vector
+
+    /*
+
+        Escopo da função membro sort_vector():
+        A função sort_vector() receberá por parâmetro o vector contendo ponteiros para pair, o modo de ordenação processado pela classe inp::processInputs e 
+        ordenará o vector de ponteiros da forma desejada, para posteriormente retorná-lo para outra classe responsável por enviar as palavras para um arquivo de saída.
+    
+    */
+
+    void sort_vector(std::vector<std::pair<const std::wstring, int>*> vet, std::string modo){
+
+
+    }
+
+
+
+
+
+} //namespace mid
---
+++ src/processMap.hpp
@@ -0,0 +1,51 @@
+/*
+                                                        CLASSE processMap
+
+    A classe processMap será uma classe intermediária, tendo o objetivo de basicamente receber "ordens" da classe inp::processInputs, como se ela fosse uma classe "slave"
+    Ela receberá o map que fora processado anteriormente, e o lerá e o colocará em um vector de ponteiros para pairs.
+    Após inserir os dados em um vector de ponteiros para pairs, a classe em questão ordenará o vector, baseado no que fora (ou não) passado pelo usuário via terminal.
+    Após deixar o vector ordenado, o bastão será passado para a(s) classe(s) de saída.
+    Essa classe terá o namespace mid, pois representa o "meio" do programa.
+
+*/
+#ifndef PROCESS_MAP_HPP
+#define PROCESS_MAP_HPP
+
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <map>
+#include <iostream>
+
+namespace mid{
+
+    class processMap{
+
+        private:
+
+        std::vector<std::pair<const std::wstring, int>*> pointer_vector;
+        
+        public:
+
+        processMap();
+        
+        void pushMap(std::map<std::wstring, int> palavrasLidas);
+
+        void sort_vector(std::vector<std::pair<const std::wstring, int>*> vet, std::string modo);
+
+        std::vector<std::pair<const std::wstring, int>*> get_ordered_vector(){
+            
+            if(pointer_vector.size() > 0){
+                return pointer_vector;
+            }
+            else{
+                std::cout << "Não há elementos no vector em questão!" << std::endl;
+                return;
+            }
+         }
+                
+    };
+
+} //namespace mid
+
+#endif
