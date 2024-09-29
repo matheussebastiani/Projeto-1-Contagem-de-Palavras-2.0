5cca6dcf67d1dc19bb0fa6261a2a8b8c3bb39f12
Rosset03 <rosset03gustavo@gmail.com>
2024-09-28 22:38:42 -0300

    Criada a classe de saida

---
+++ src/processOutputs.hpp
@@ -0,0 +1,45 @@
+/*
+                                 ---Função da classe---
+
+A classe a seguir recebe um VECTOR contendo ponteiros para PAIR ordenados na ordem escolhida pelo usuário
+e o converterá em um arquivo de saída, tambem escolhido pelo usuário,
+podendo ser CSV ou HTML
+
+
+
+
+*/
+
+#ifndef PROCESSOUTPUTS_HPP
+#define PROCESSOUTPUTS_HPP
+
+#include <vector>
+#include <string>
+
+
+class processOutputs{
+    private:
+        std::vector<std::pair<const std::wstring, int>*> vector_saida;
+    public:
+        void export_html();
+        void export_csv();
+
+
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
+
